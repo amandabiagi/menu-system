645e43b7901a0e26377e628e00c1b81631b470ea
amanda.biagi <amanda.biagi@bandtec.com.br>
2025-03-13 08:36:40 -0300

    Adicionando a estrutura inicial do projeto backend

---
+++ menu-system-api/menu-system/src/test/java/com/biagi/menu_system/MenuSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biagi.menu_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
